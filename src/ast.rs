a15767b8671ca22dfcb33ec611637cc76c16b583
ridho9 <p.ridho9@gmail.com>
2024-02-28 15:58:49 +0800

    refactor

--- src/ast.rs
+++ src/ast.rs
@@ -1,5 +1,61 @@
 use std::{ops::Add, vec};
 
+#[derive(Debug, Clone, Copy)]
+pub enum Node {
+    Nil,
+    Bool(bool),
+    Number(f64),
+    BinaryOp(BinaryOp, NodeRef, NodeRef),
+    UnaryOp(UnaryOp, NodeRef),
+}
+
+impl Add<usize> for Node {
+    type Output = Node;
+
+    fn add(self, t: usize) -> Self::Output {
+        use Node::*;
+        match self {
+            Nil => Nil,
+            Bool(b) => Bool(b),
+            Number(n) => Number(n),
+            BinaryOp(op, lhs, rhs) => BinaryOp(op, lhs + t, rhs + t),
+            UnaryOp(op, i) => UnaryOp(op, i + t),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum BinaryOp {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    EqEq,
+    Neq,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum UnaryOp {
+    Neg,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct NodeRef(pub usize);
+
+impl Add<usize> for NodeRef {
+    type Output = NodeRef;
+
+    fn add(self, rhs: usize) -> Self::Output {
+        NodeRef(self.0 + rhs)
+    }
+}
+
+impl Into<NodeRef> for usize {
+    fn into(self) -> NodeRef {
+        NodeRef(self)
+    }
+}
+
 #[derive(Debug)]
 pub struct NodeList(pub Vec<Node>);
 
@@ -19,53 +75,9 @@ impl NodeList {
     pub fn append(&mut self, rhs: NodeList) -> NodeRef {
         let start_ref = self.0.len();
         for mut node in rhs.0 {
-            node.transpose(start_ref);
+            node = node + start_ref;
             self.0.push(node);
         }
         self.noderef()
     }
-}
-
-#[derive(Debug, Clone, Copy)]
-pub struct NodeRef(pub usize);
-
-impl Add<usize> for NodeRef {
-    type Output = NodeRef;
-
-    fn add(self, rhs: usize) -> Self::Output {
-        NodeRef(self.0 + rhs)
-    }
-}
-
-#[derive(Debug, Clone, Copy)]
-pub enum Node {
-    Nil,
-    Bool(bool),
-    Number(f64),
-    BinaryOp(BinaryOp, NodeRef, NodeRef),
-    UnaryOp(UnaryOp, NodeRef),
-}
-impl Node {
-    fn transpose(&mut self, t: usize) {
-        *self = match *self {
-            Node::BinaryOp(op, lhs, rhs) => Node::BinaryOp(op, lhs + t, rhs + t),
-            Node::UnaryOp(op, i) => Node::UnaryOp(op, i + t),
-            node => node,
-        }
-    }
-}
-
-#[derive(Debug, Clone, Copy)]
-pub enum BinaryOp {
-    Add,
-    Sub,
-    Mul,
-    Div,
-    EqEq,
-    Neq,
-}
-
-#[derive(Debug, Clone, Copy)]
-pub enum UnaryOp {
-    Neg,
 }
